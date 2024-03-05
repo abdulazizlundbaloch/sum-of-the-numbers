3ba29f35ba6f0c1211739ee21bdf8c8391376a13
abdulazizlundbaloch <162303594+abdulazizlundbaloch@users.noreply.github.com>
2024-03-05 14:49:54 +0500

    Add files via upload

---
+++ area of the triangle.py
@@ -0,0 +1,8 @@
+#practice problems
+#1:: write a python script to find the area of the area of the triangle
+#area of triangle=1/2*base*hypotenuse
+base_of_the_triangle=int(input("Enter the base of the triangle: "))
+hypotenuse_of_the_triangle=int(input("Enter the hypotenuse of the triangle: "))
+area_of_the_triangle=(base_of_the_triangle)*(hypotenuse_of_the_triangle)*1/2
+print("There exists a triangle whose base is",base_of_the_triangle,"and hypotnuse is",hypotenuse_of_the_triangle,"and area is",area_of_the_triangle)
+5
